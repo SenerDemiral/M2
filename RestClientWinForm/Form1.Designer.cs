90e79d9e1997968bb9b86cce5e487690309761f6
SenerDemiral <sener.demiral@gmail.com>
2018-05-25 14:43:27 +0300

    Added DateSet to Client

--- RestClientWinForm/Form1.Designer.cs
+++ RestClientWinForm/Form1.Designer.cs
@@ -29,6 +29,24 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataTable1 = new System.Data.DataTable();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataColumn2 = new System.Data.DataColumn();
+            this.dataColumn3 = new System.Data.DataColumn();
+            this.dataColumn4 = new System.Data.DataColumn();
+            this.dataColumn5 = new System.Data.DataColumn();
+            this.dataColumn6 = new System.Data.DataColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.oNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldStrDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldIntDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldDblDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldDcmDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -41,14 +59,124 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            this.dataSet1.Tables.AddRange(new System.Data.DataTable[] {
+            this.dataTable1});
+            // 
+            // dataTable1
+            // 
+            this.dataTable1.Columns.AddRange(new System.Data.DataColumn[] {
+            this.dataColumn1,
+            this.dataColumn2,
+            this.dataColumn3,
+            this.dataColumn4,
+            this.dataColumn5,
+            this.dataColumn6});
+            this.dataTable1.Constraints.AddRange(new System.Data.Constraint[] {
+            new System.Data.UniqueConstraint("Constraint1", new string[] {
+                        "ONo"}, false)});
+            this.dataTable1.TableName = "Table1";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.ColumnName = "ONo";
+            this.dataColumn1.DataType = typeof(ulong);
+            this.dataColumn1.ReadOnly = true;
+            // 
+            // dataColumn2
+            // 
+            this.dataColumn2.ColumnName = "fldStr";
+            // 
+            // dataColumn3
+            // 
+            this.dataColumn3.ColumnName = "fldInt";
+            this.dataColumn3.DataType = typeof(int);
+            // 
+            // dataColumn4
+            // 
+            this.dataColumn4.ColumnName = "fldDbl";
+            this.dataColumn4.DataType = typeof(double);
+            // 
+            // dataColumn5
+            // 
+            this.dataColumn5.ColumnName = "fldDcm";
+            this.dataColumn5.DataType = typeof(decimal);
+            // 
+            // dataColumn6
+            // 
+            this.dataColumn6.ColumnName = "fldDate";
+            this.dataColumn6.DataType = typeof(System.DateTime);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.oNoDataGridViewTextBoxColumn,
+            this.fldStrDataGridViewTextBoxColumn,
+            this.fldIntDataGridViewTextBoxColumn,
+            this.fldDblDataGridViewTextBoxColumn,
+            this.fldDcmDataGridViewTextBoxColumn,
+            this.fldDateDataGridViewTextBoxColumn});
+            this.dataGridView1.DataMember = "Table1";
+            this.dataGridView1.DataSource = this.dataSet1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 112);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 338);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // oNoDataGridViewTextBoxColumn
+            // 
+            this.oNoDataGridViewTextBoxColumn.DataPropertyName = "ONo";
+            this.oNoDataGridViewTextBoxColumn.HeaderText = "ONo";
+            this.oNoDataGridViewTextBoxColumn.Name = "oNoDataGridViewTextBoxColumn";
+            this.oNoDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // fldStrDataGridViewTextBoxColumn
+            // 
+            this.fldStrDataGridViewTextBoxColumn.DataPropertyName = "fldStr";
+            this.fldStrDataGridViewTextBoxColumn.HeaderText = "fldStr";
+            this.fldStrDataGridViewTextBoxColumn.Name = "fldStrDataGridViewTextBoxColumn";
+            // 
+            // fldIntDataGridViewTextBoxColumn
+            // 
+            this.fldIntDataGridViewTextBoxColumn.DataPropertyName = "fldInt";
+            this.fldIntDataGridViewTextBoxColumn.HeaderText = "fldInt";
+            this.fldIntDataGridViewTextBoxColumn.Name = "fldIntDataGridViewTextBoxColumn";
+            // 
+            // fldDblDataGridViewTextBoxColumn
+            // 
+            this.fldDblDataGridViewTextBoxColumn.DataPropertyName = "fldDbl";
+            this.fldDblDataGridViewTextBoxColumn.HeaderText = "fldDbl";
+            this.fldDblDataGridViewTextBoxColumn.Name = "fldDblDataGridViewTextBoxColumn";
+            // 
+            // fldDcmDataGridViewTextBoxColumn
+            // 
+            this.fldDcmDataGridViewTextBoxColumn.DataPropertyName = "fldDcm";
+            this.fldDcmDataGridViewTextBoxColumn.HeaderText = "fldDcm";
+            this.fldDcmDataGridViewTextBoxColumn.Name = "fldDcmDataGridViewTextBoxColumn";
+            // 
+            // fldDateDataGridViewTextBoxColumn
+            // 
+            this.fldDateDataGridViewTextBoxColumn.DataPropertyName = "fldDate";
+            this.fldDateDataGridViewTextBoxColumn.HeaderText = "fldDate";
+            this.fldDateDataGridViewTextBoxColumn.Name = "fldDateDataGridViewTextBoxColumn";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +184,21 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Data.DataSet dataSet1;
+        private System.Data.DataTable dataTable1;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataColumn dataColumn2;
+        private System.Data.DataColumn dataColumn3;
+        private System.Data.DataColumn dataColumn4;
+        private System.Data.DataColumn dataColumn5;
+        private System.Data.DataColumn dataColumn6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn oNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldStrDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldIntDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldDblDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldDcmDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldDateDataGridViewTextBoxColumn;
     }
 }
 
--- RestClientWinForm/Form1.cs
+++ RestClientWinForm/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -28,7 +29,15 @@ namespace RestClientWinForm
 
         async void Test()
         {
-            //var dt = dataSet1.Tables[0];
+            //Dynamically create Table from class.
+            //Not fullfil our requirements. Table structure should be created at design time
+            //to use in Controls. Create maunally now.
+            //var aaaa = CreateDataTable<TblaRec>();
+            //dataSet1.Tables.Add(aaaa);
+
+
+            var dt = dataSet1.Tables[0];
+            dt.BeginLoadData();
             StringBuilder sb = new StringBuilder();
             int nor = 0, ml = 0;
             Channel channel = new Channel($"127.0.0.1:50051", ChannelCredentials.Insecure);
@@ -36,6 +45,7 @@ namespace RestClientWinForm
             var client = new CRUDs.CRUDsClient(channel);
             CancellationToken token = new CancellationToken();
 
+            DataRow row;
             Stopwatch sw = new Stopwatch();
             sw.Start();
             using (var response = client.SelectTbla(new TblaQry { Query = "abc" }))
@@ -43,7 +53,15 @@ namespace RestClientWinForm
                 while (await response.ResponseStream.MoveNext(token))
                 {
                     var rec = response.ResponseStream.Current;
-                    //dt.Rows.Add(rec.Message);
+                    row = dt.NewRow();
+                    row["Ono"] = rec.Ono;
+                    row["fldStr"] = rec.FldStr;
+                    row["fldInt"] = rec.FldInt;
+                    row["fldDbl"] = rec.FldDbl;
+                    row["fldDate"] = new DateTime(rec.FldDateTicks);
+
+
+                    dt.Rows.Add(row);
 
                     sb.AppendLine(rec.Ono.ToString());
                     nor++;
@@ -51,9 +69,27 @@ namespace RestClientWinForm
                     //MessageBox.Show(rec.Message);
                 }
             }
+            dt.EndLoadData();
             sw.Stop();
             //MessageBox.Show(sb.ToString());
             MessageBox.Show($"Time elapsed: {nor:n0}recs  {sw.ElapsedMilliseconds:n0}ms  {nor / sw.ElapsedMilliseconds}recs/ms TotalSize:{ml:n0}");
+        }
+
+        public static DataTable CreateDataTable<T>()
+        {
+            var dt = new DataTable();
+
+            var propList = typeof(T).GetMembers(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+
+            foreach (MemberInfo info in propList)
+            {
+                if (info is PropertyInfo)
+                    dt.Columns.Add(new DataColumn(info.Name, (info as PropertyInfo).PropertyType));
+                else if (info is FieldInfo)
+                    dt.Columns.Add(new DataColumn(info.Name, (info as FieldInfo).FieldType));
+            }
+
+            return dt;
         }
     }
 }
